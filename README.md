7cb296bbc1883487c2d90b2f7f12402b8e48b788
Taylor Marvin <Taylor@Marvin.email>
2021-06-11 20:37:28 -0700

    Profile & VSCode Settings

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Settings
-VSCode/PS Settings
+
+```
+The VSCode settings.json file usually lives: %appdata%\Code\User\settings.json
+```
+
+<br>
+
+# Profile
+
+<p>
+
+Profile Paths:
+```
+$PROFILE.CurrentUserAllHosts
+$PROFILE.CurrentUserCurrentHost
+$PROFILE.AllUsersCurrentHost
+$PROFILE.AllUsersAllHosts
+Linux VSCode: ~/.config/powershell/Microsoft.VSCode_profile.ps1
+```
+
+Quickly find which profile paths exist:
+```
+Foreach ($Path in (
+		$PROFILE.CurrentUserAllHosts,
+		$PROFILE.CurrentUserCurrentHost,
+		$PROFILE.AllUsersCurrentHost,
+		$PROFILE.AllUsersAllHosts,
+		'~/.config/powershell/Microsoft.VSCode_profile.ps1'
+	)
+){ Get-Item $Path -ErrorAction Ignore }
+```
+</p>
---
+++ profile.ps1
@@ -0,0 +1,83 @@
+Function Prompt {
+	Begin {
+		$PathSep = [IO.Path]::DirectorySeparatorChar
+		$TimeStamp = (Get-Date -Format 'yyyyMMddHHmmsszz').ToString()
+	}
+
+	Process {
+		if (-Not $env:SetWindowTitle) {
+			Set-WindowTitle
+			$env:SetWindowTitle = $true
+		}
+		$CurrentPath = $executionContext.SessionState.Path.CurrentLocation.Path
+
+		if ($env:AdminContext) {
+			# Add admin to command prompt
+			Write-Host '[' -NoNewline -ForegroundColor White
+			Write-Host 'Admin' -NoNewline -ForegroundColor Red
+			Write-Host '] ' -NoNewline -ForegroundColor White
+		}
+
+		Write-Host "$TimeStamp " -NoNewline
+		if ($pwd.Provider.Name -ne 'FileSystem') {
+			# Add Provider type to prompt if its not filesystem.
+			Write-Host '[' -NoNewline -ForegroundColor White
+			Write-Host $pwd.Provider.Name -NoNewline -ForegroundColor Green
+			Write-Host '] ' -NoNewline -ForegroundColor White
+		}
+		if ($CurrentPath.Split("$PathSep").count -lt 4) {
+			$OutputPath = "$CurrentPath>"
+		} else {
+			if (($CurrentPath.StartsWith('\\')) -and (($CurrentPath.Split('\')).count -ge 6)) {
+				$OutputPath = ("\\$($CurrentPath.Split('\')[2])\...\$($CurrentPath.Split('\')[-2])\$($CurrentPath.Split('\')[-1])>")
+			} else {
+				$OutputPath = "$($CurrentPath.Split("$PathSep")[0])$PathSep...$PathSep$($CurrentPath.Split("$PathSep")[-2])$PathSep$($CurrentPath.Split("$PathSep")[-1])>"
+			}
+		}
+	}
+
+	End { "$OutputPath" }
+}
+
+Function Set-WindowTitle {
+	Begin {
+		if (-Not $IsLinux) {
+			$AdminCheck = [Security.Principal.WindowsPrincipal]::New(
+				[Security.Principal.WindowsIdentity]::GetCurrent()
+			).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+		}
+		$WindowTitle = ''
+	}
+
+	Process {
+		# Build WindowTitle
+		if ($AdminCheck) {
+			# Don't start in System32 by default.
+			if ($executionContext.SessionState.Path.CurrentLocation.Path -ieq 'C:\Windows\System32') {
+				Set-Location ~
+			}
+			$Env:AdminContext = $True
+			$WindowTitle = '[Admin] '
+			# Add admin to command prompt since it was missed on first startup.
+		}
+
+		# Add PSEdition to WindowTitle
+		$WindowTitle += Switch ($PSVersionTable.PSEdition) {
+			'Core' { 'PWSH' }
+			'Desktop' { 'PS' }
+			Default {}
+		}
+
+		# Add PSVersion number and current provider to WindowTitle
+		$WindowTitle += " v$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Patch)" # Done this way for win PS compat.
+		$WindowTitle += " [$($pwd.Provider.Name)]"
+
+		# Update window title
+		Try {
+			$Host.Ui.RawUi.WindowTitle = "$WindowTitle"
+		} Catch {
+			# Do Nothing.
+		}
+	}
+	End {}
+}
